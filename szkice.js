af7e76abe0e3793d138209e6b3d76c948c9781ff
improcode <art.milkowski@gmail.com>
2018-05-25 14:47:25 +0200

    first part: css and common

---
+++ szkice.js
@@ -0,0 +1,9 @@
+
+
+//unige numbers
+
+//     _arrayOfNumbers.map((element)=> ({
+//     for (var i = 0, i <= _arrayOfNumbers.length, i++) {
+//         if (element == _arrayOfNumbers[i]) return
+//     }
+// }))
--- test.common.js
+++ test.common.js
@@ -1,24 +1,47 @@
 // min 2-3 tests for every function 
 
-const _unique = (_arrayOfNumbers)=>{
+const _unique = (_arrayOfNumbers) => {
     // return only unique numbers in new array
+
+    return _arrayOfNumbers.filter((value, index, self) => (self.indexOf(value) === index))
+
+
 }
 
-const _isOdd = (value)=>{
+const _isOdd = (value) => {
     // if value is odd then true
+    if (value % 2 === 0) {
+        return true
+    } else {
+        return false
+    }
+
 }
 
-const _isEmail = (str)=>{
+const _isEmail = (str) => {
     // *if str is email-like then true 
     // find regexp on stackoverflow :)
+
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(str.toLowerCase());
+
 }
 
-const _mapForArray = (_array, callback)=>{
+
+const _mapForArray = (_array, callback) => {
     // use for/while loop
     // same functionalty as map
+    var _arrayAfterMap = []
+    for (var i = 0; i<_array.length; i++) {
+        _arrayAfterMap.push(callback(_array[i]))
+    }
+    return _arrayAfterMap
 }
 
-const _mapForObject = (_object, callback)=>{
+const _mapForObject = (_object, callback) => {
     // *use for/while loop
     // same functionalty as map but for object values
+
+
+
 }
